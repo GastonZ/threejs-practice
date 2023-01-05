73ed66d851b22690a571da2b9cb0e0bbaef38d3e
GastonZ <gastonzappullaryu@gmail.com>
2023-01-05 08:25:37 -0300

    please images

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load('gastonzappullaport.jpg');
+const jeffTexture = new THREE.TextureLoader().load('../gastonzappullaport.jpg');
 
 const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
 
@@ -74,7 +74,7 @@ scene.add(jeff);
 // Moon
 
 const moonTexture = new THREE.TextureLoader().load('https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Moon_texture.jpg/720px-Moon_texture.jpg');
-const normalTexture = new THREE.TextureLoader().load('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d71d8fb9-465b-4438-9ea6-7e93d49edd68/d7lpe0w-6b63d757-1367-4daa-808e-74c1a9fe9c8a.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q3MWQ4ZmI5LTQ2NWItNDQzOC05ZWE2LTdlOTNkNDllZGQ2OFwvZDdscGUwdy02YjYzZDc1Ny0xMzY3LTRkYWEtODA4ZS03NGMxYTlmZTljOGEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.azAMqy6AuXByty6fJ3KFVe3V1ne3J861QLVcBo6wctw');
+const normalTexture = new THREE.TextureLoader().load('../normal.jpg');
 
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
